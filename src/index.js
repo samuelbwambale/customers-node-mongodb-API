6c279ae1a8142a53f193f22024a16ef729d9a2a7
samuelbwambale <simplesam63@gmail.com>
2020-01-03 12:27:39 -0600

    add a middleware to log the routes hit in the API

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ const express = require('express')
 const app = express()
 
 const personRoute = require('./routes/person')
+
+// the order of adding the middlewares matters
+
+// logging middleware
+app.use((req, res, next) => {
+    console.log(`${new Date().toString()} => ${req.originalUrl}`)
+
+    next()
+})
+
 // register the person route
 app.use(personRoute)
 app.use(express.static('public'))
