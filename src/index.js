8ebe8f05a4272aa49e3ae92d2d6e4b68e10714bd
samuelbwambale <simplesam63@gmail.com>
2020-01-03 11:15:10 -0600

    initialize node project configure express to serve static content

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let express = require('express')
+
+let app = express()
+
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server has started on ${PORT}`))
