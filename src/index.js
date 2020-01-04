3eb192701fd5d8ad63059896eaa1f65546775f60
samuelbwambale <simplesam63@gmail.com>
2020-01-03 22:19:47 -0600

    add error handling middleware - 400 error - 500 error

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+const path = require('path')
+
 const personRoute = require('./routes/person')
 
 // the order of adding the middlewares matters
@@ -16,6 +18,18 @@ app.use((req, res, next) => {
 // register the person route
 app.use(personRoute)
 app.use(express.static('public'))
+
+// Error middleware typically at the end
+// Handler for 404 - Resource Not Found
+app.use((req, res, next) => {
+    res.status(404).send('Resource Not Found')
+  })
+
+// Handler for Error 500
+app.use((err, req, res, next) => {
+    // res.status(500).send(err.stack)
+    res.sendFile(path.join(__dirname, '../public/500.html'))
+  })
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server has started on ${PORT}`))
